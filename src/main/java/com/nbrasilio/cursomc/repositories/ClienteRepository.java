3a352120eb54f84b8369e325f5bfaf148ede2b2b
Nelson Brasilio <nbrasilio@hotmail.com>
2019-01-19 18:21:42 -0200

    Cliente, tipo cliente, telefones e enderecos

---
+++ src/main/java/com/nbrasilio/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.nbrasilio.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nbrasilio.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
